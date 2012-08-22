99240332db27442166598706380106114011d5f5
Hermes Ojeda Ruiz <hermes.ojeda@logicalbricks.com>
2012-08-21 20:44:30 -0500

    Creada página de visualización de un único evento, closes #8, por @gmjorge y @hermes-logicalbricks

--- app/uploaders/foto_uploader.rb
+++ app/uploaders/foto_uploader.rb
@@ -42,7 +42,7 @@ class FotoUploader < CarrierWave::Uploader::Base
 
 
   version :medium do
-    process :resize_and_pad => [500, 500]
+    process :resize_and_pad => [560, 560]
   end
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
