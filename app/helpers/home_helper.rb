dbca614211d6aa8ec7ea7e852a02e9de559d3484
Azarel Doroteo Pacheco <azarel.doroteo@logicalbricks.com>
2012-08-25 18:33:51 -0500

    Se limpia un poco el código utilzando helpers

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,19 @@
 module HomeHelper
+  def link_to_next_month date
+    date = date.next_month
+    link_to build_month_name(date), build_calendario_path(date)
+  end
+
+  def link_to_previous_month date
+    date = date.prev_month
+    link_to build_month_name(date), build_calendario_path(date)
+  end
+
+  def build_month_name date
+    (l @date.prev_month, format: :nombre_mes)
+  end
+
+  def build_calendario_path date
+    "/calendario/#{date.prev_month.year}/#{@date.prev_month.month}"
+  end
 end
