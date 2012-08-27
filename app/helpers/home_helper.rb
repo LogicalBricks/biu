7dce4625f7c926fed34c08e92ca8e00a6e8cb2f7
Hermes Ojeda Ruiz <hermes.ojeda@logicalbricks.com>
2012-08-27 18:29:47 -0500

    Agregados botones para el calendario

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,12 +1,12 @@
 module HomeHelper
   def link_to_next_month date
     date = date.next_month
-    link_to build_month_name(date), build_calendario_path(date)
+    link_to content_tag(:i,'',class: 'icon-chevron-right'), build_calendario_path(date)
   end
 
   def link_to_previous_month date
     date = date.prev_month
-    link_to build_month_name(date), build_calendario_path(date)
+    link_to content_tag(:i,'',class: 'icon-chevron-left'), build_calendario_path(date)
   end
 
   def build_month_name date
