1fa49dbe8182b2eeca4e7d9227b0405599dc3c89
Hermes Ojeda Ruiz <hermes.ojeda@logicalbricks.com>
2012-08-27 19:31:23 -0500

    Agregada funcionalidad para que muestre en link del evento/lugar/etc en el mapa

--- app/models/localizacion.rb
+++ app/models/localizacion.rb
@@ -2,4 +2,9 @@ class Localizacion < ActiveRecord::Base
   attr_accessible :latitude, :longitude
   acts_as_gmappable process_geocoding: false 
   belongs_to :localizable, polymorphic: true
+
+  def gmaps4rails_infowindow 
+    objeto = self.localizable
+    link = "<a href='/#{objeto.class.name.downcase.pluralize}/#{objeto.id}'>#{objeto.nombre}</a>"
+  end
 end
